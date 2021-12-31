1209ae5d1d10d6b31a9d161c54d033e130c46a5f
Edward Knueppel <edward.knueppel@zywave.com>
2021-12-30 20:34:42 -0600

    Add project files.

---
+++ SimonGame/SimonGame/GameDriver.cs
@@ -0,0 +1,26 @@
+﻿namespace SimonGame
+{
+    using System;
+
+    using Xamarin.Forms;
+
+    internal sealed class GameDriver
+    {
+        const int maxRoundLength = 20;
+        Button[] playerButtons = new Button[maxRoundLength];
+        int numberOfButtonPresses = 0;
+
+        public GameDriver()
+        {
+
+        }
+
+        internal void AddButton(Button button)
+        {
+            playerButtons[numberOfButtonPresses++] = button;
+            Console.WriteLine(playerButtons.ToString());
+        }
+
+
+    }
+}
---
+++ SimonGame/SimonGame/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿namespace SimonGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Xamarin.Forms;
+
+    public partial class MainPage : ContentPage
+    {
+        private List<Button> _buttons = new List<Button>();
+        private GameDriver _game;
+        public MainPage()
+        {
+            InitializeComponent();
+            _buttons.Add(greenButton);
+            _buttons.Add(blueButton);
+            _buttons.Add(redButton);
+            _buttons.Add(yellowButton);
+
+            _game = new GameDriver();            
+        }
+
+        public async void GreenButtonClicked(object sender, EventArgs e)
+        {
+            _game.AddButton(sender as Button);
+            _ = StartAnimation();
+        }
+
+        public void RedButtonClicked(object sender, EventArgs e)
+        {
+            _game.AddButton(sender as Button);
+        }
+
+        public void YellowButtonClicked(object sender, EventArgs e)
+        {
+            _game.AddButton(sender as Button);
+        }
+
+        public void BlueButtonClicked(object sender, EventArgs e)
+        {
+            _game.AddButton(sender as Button);
+        }
+
+        public void PlayButtonClicked(object sender, EventArgs e)
+        {
+            playButton.Text = playButton.Text == "Play" ? "Pause Game" : "Play";
+        }
+
+        private async Task StartAnimation()
+        {
+            await Task.Delay(200);
+            await greenButton.FadeTo(0, 250);
+            await Task.Delay(200);
+            await greenButton.FadeTo(1, 250);
+        }
+    }
+}
